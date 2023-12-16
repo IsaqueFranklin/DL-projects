a48ece908ed1b06ab525a78b00245f714a2c5028
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2023-12-15 21:38:06 -0300

    modification digit-classifier

--- digit-classifier-fromScracth.py
+++ digit-classifier-fromScracth.py
@@ -1,2 +1,34 @@
-print('haha');
-print('Only for making my github full today.')
+import fastbook
+fastbook.setup_book()
+from fastai.vision.all import *
+from fastbook import *
+
+matplotlib.rc('image', cmap='Greys')
+path = untar_data(URLs.MNIST_SAMPLE)
+Path.BASE_PATH = path
+
+path.ls()
+(path/'train').ls()
+threes = (path/'train'/'3').ls().sorted()
+sevens = (path/'train'/'7').ls().sorted()
+threes
+im3_path = threes[1]
+im3 = Image.open(im3_path)
+im3
+
+array(im3)[4:10,4:10]
+tensor(im3)[4:10,4:10]
+
+im3_t = tensor(im3)
+df = pd.DataFrame(im3_t[4:15,4:22])
+df.style.set_properties(**{'font-size':'6pt'}).background_gradient('Greys')
+seven_tensors = [tensor(Image.open(o)) for o in sevens]
+three_tensors = [tensor(Image.open(o)) for o in threes]
+len(three_tensors),len(seven_tensors)
+
+show_image(three_tensors[1]);
+stacked_sevens = torch.stack(seven_tensors).float()/255
+stacked_threes = torch.stack(three_tensors).float()/255
+stacked_threes.shape
+
+len(stacked_threes.shape)
